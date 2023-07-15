e0c1d9f9c91079ed713248ebab8b58b779162fe2
Shine-Herry <zhanghangrui_pr@163.com>
2023-07-15 13:02:57 +0800

    main

--- ex.py
+++ ex.py
@@ -1,4 +1,15 @@
-def make_adder(n):
-    def adder(k):
-        return k + n
-    return adder 
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            print('False')
+            return False
+        i += 1
+    print('True')
+    return True
+
+is_prime(17)
+     
