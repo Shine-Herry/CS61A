c00a356006f0d326ac055e3509409125f4fabb2c
Shine-Herry <zhanghangrui_pr@163.com>
2023-07-15 21:37:52 +0800

    mian

--- discussion/disc01.py
+++ discussion/disc01.py
@@ -1,3 +1,16 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            print('False')
+            return False
+        i += 1
+    print('True')
+    return True
+
 def fizzbuzz(n):
     if n > 0:
         i = 1
